a7b412d961e373c0c71865046edb000014487f12
frincon15 <162913163+frincon15@users.noreply.github.com>
2025-10-31 20:24:36 +0000

    Conclusão Carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -22,35 +22,45 @@ int main(int argc, char const *argv[]) {
   // Área para os dados da Carta 1
 
   printf ("Carta 1\n");
+
   printf ("Digite o Estado: \n");
-  scanf ("%s", &estado);
+  gets (estado);
+  fflush (stdin);  
 
   printf ("Digite Código da Carta: \n");
-  scanf ("%s", &codigo);
+  gets (codigo);
+  fflush (stdin);  
+
 
   printf ("Digite o nome da Cidade: \n");
-    scanf ("%s", &nome);
+  gets (nome);
+  fflush (stdin);  
 
   printf ("Digite a população: \n");
   scanf ("%d", &populacao);
+  
 
   printf ("Digite a área em Km2: \n");
   scanf ("%f", &area);
 
+
   printf ("Digite o PIB: \n");
   scanf ("%f", &pib);
+  
 
   printf ("Digite o o número de pontos turísticos: \n");
   scanf ("%d", &pontosTuristicos);
 
 
 
+
   printf ("Carta 1 \n");
 
-  printf ("Estado: %s \n", estado);
-  printf ("Código: %s \n", codigo);
-  printf ("Nome da cidade: %s \n", nome);
-  printf ("População: %d \n", populacao);
+  printf ("Estado: "); puts (estado);
+  printf ("Código: "); puts (codigo);
+  printf ("Nome da cidade: "); puts (nome);
+ 
+  printf ("Populacão: %d\n", populacao);
   printf ("Área: %.2f ", area);
   printf ("Km2\n");
   printf ("PIB: %.2f ", pib);
