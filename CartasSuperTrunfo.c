a03df4443ddec297dc1b2290752cf20f25254f34
frincon15 <162913163+frincon15@users.noreply.github.com>
2025-10-31 05:07:06 +0000

    Carta 1 Atualização

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
+int main(int argc, char const *argv[]) {
   // Descrição do desafio 
   printf ("Desafio Super Trunfo - Países\n");
 
@@ -27,7 +29,7 @@ int main() {
   scanf ("%s", &codigo);
 
   printf ("Digite o nome da Cidade: \n");
-  scanf ("%s", &nome);
+    scanf ("%s", &nome);
 
   printf ("Digite a população: \n");
   scanf ("%d", &populacao);
@@ -35,9 +37,27 @@ int main() {
   printf ("Digite a área em Km2: \n");
   scanf ("%f", &area);
 
+  printf ("Digite o PIB: \n");
+  scanf ("%f", &pib);
+
+  printf ("Digite o o número de pontos turísticos: \n");
+  scanf ("%d", &pontosTuristicos);
+
+
+
   printf ("Carta 1 \n");
 
-  printf ("O Estado é: %s \n", estado);
+  printf ("Estado: %s \n", estado);
+  printf ("Código: %s \n", codigo);
+  printf ("Nome da cidade: %s \n", nome);
+  printf ("População: %d \n", populacao);
+  printf ("Área: %.2f ", area);
+  printf ("Km2\n");
+  printf ("PIB: %.2f ", pib);
+  printf ("bilhões de reais\n");
+  printf ("Número de pontos turísticos: %d \n", pontosTuristicos);
+
+ 
 
 
 
