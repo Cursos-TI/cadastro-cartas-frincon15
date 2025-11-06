eaf4655097f917266930f9dc230ac8a9dd9c82b6
frincon15 <162913163+frincon15@users.noreply.github.com>
2025-11-06 03:29:52 +0000

    Inclusão de densidade e pib per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,45 +16,109 @@ int main(int argc, char const *argv[]) {
   char codigo [50];
   char nome [50];
   int populacao, pontosTuristicos;
-  float area, pib;
+  float area, pib, densidade, pibpc;
   
   // Área para perguntas ao usuário sobre as informações da cidade e armazenamento das informações
-  // Área para os dados da Carta 1
+
+  
+    // Área para a Carta 1
+
 
   printf ("Carta 1\n");
 
-  printf ("Digite o Estado: \n");
+  printf ("Digite o Estado: ");
   gets (estado);
   fflush (stdin);  
-
-  printf ("Digite Código da Carta: \n");
+    
+  printf ("Digite Código da Carta: ");
   gets (codigo);
   fflush (stdin);  
 
-
-  printf ("Digite o nome da Cidade: \n");
+  printf ("Digite o nome da Cidade: ");
   gets (nome);
   fflush (stdin);  
 
-  printf ("Digite a população: \n");
+  printf ("Digite a população: ");
   scanf ("%d", &populacao);
   
-
-  printf ("Digite a área em Km2: \n");
+  printf ("Digite a área em Km2: ");
   scanf ("%f", &area);
 
-
-  printf ("Digite o PIB: \n");
+  printf ("Digite o PIB: ");
   scanf ("%f", &pib);
   
-
-  printf ("Digite o o número de pontos turísticos: \n");
+  printf ("Digite o o número de pontos turísticos: ");
   scanf ("%d", &pontosTuristicos);
 
+ 
+
+  // Área para exibição dos dados da cidade
 
 
 
   printf ("Carta 1 \n");
+
+  printf ("Estado: "); puts (estado);
+  printf ("Código: "); puts (codigo);
+  printf ("Nome da cidade: "); puts (nome);
+  printf ("Populacão: %d\n", populacao);
+  printf ("Área: %.2f ", area);
+  printf ("Km2\n");
+  printf ("PIB: %.2f ", pib);
+  printf ("bilhões de reais\n");
+  printf ("Número de pontos turísticos: %d \n", pontosTuristicos);
+
+  densidade = populacao/area;
+	
+	printf ("A densidade populacional do estado é: %.2f ", densidade);
+	printf ("hab/Km2\n");
+	
+	pibpc = pib/populacao;
+	printf ("O PIB per capita é de: %.2f ", pibpc);
+	printf ("R$/habitante\n");
+	
+  
+  
+
+
+
+    // Área para a Carta 2
+
+        // Carta 2
+
+  printf ("Carta 2\n");
+   
+
+  printf ("Digite o Estado: ");
+  gets (estado);
+  fflush (stdin);  
+    
+  printf ("Digite Código da Carta: ");
+  gets (codigo);
+  fflush (stdin);  
+
+  printf ("Digite o nome da Cidade: ");
+  gets (nome);
+  fflush (stdin);  
+
+  printf ("Digite a população: ");
+  scanf ("%d", &populacao);
+  
+
+  printf ("Digite a área em Km2: ");
+  scanf ("%f", &area);
+
+
+  printf ("Digite o PIB: ");
+  scanf ("%f", &pib);
+  
+
+  printf ("Digite o o número de pontos turísticos: ");
+  scanf ("%d", &pontosTuristicos);
+
+
+  
+  printf ("Carta 2 \n");
 
   printf ("Estado: "); puts (estado);
   printf ("Código: "); puts (codigo);
@@ -67,24 +131,23 @@ int main(int argc, char const *argv[]) {
   printf ("bilhões de reais\n");
   printf ("Número de pontos turísticos: %d \n", pontosTuristicos);
 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // Área para entrada de dados
+  densidade = populacao/area;
+	
+	printf ("A densidade populacional do estado é: %.2f ", densidade);
+	printf ("hab/Km2\n");
+	
+	pibpc = pib/populacao;
+	printf ("O PIB per capita é de: %.2f ", pibpc);
+	printf ("R$/habitante\n");
+	
+  
   
 
-  // Área para exibição dos dados da cidade
+  
 
+
+  
+
+ 
 return 0;
 } 
